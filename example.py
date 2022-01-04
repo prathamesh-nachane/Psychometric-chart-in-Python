131fa7ba3befeba5e43f5a5c4d44750435a35971
prathamesh-nachane <39289245+prathamesh-nachane@users.noreply.github.com>
2022-01-04 17:09:22 -0600

    Update example.py

--- example.py
+++ example.py
@@ -7,13 +7,13 @@
 #Read License file
 
 import matplotlib.pyplot as plt
-import psycochart as psycochart
+import psychrochart as psycochart
 
 plt.close("all") 
 figure,axes = psycochart.plot_psy_chart(x_low_limit = -10,x_upp_limit = 60,y_low_limit = 0,y_upp_limit = 0.03, p = 101325, RH_lines = 'y',H_lines = 'y',WB_lines = 'y')
 a = [[50,0.007],[40, 0.006],[30,0.003]]
 figure,axes = psycochart.plot_points(a,figure,axes, col = 'r', typ = '-', grid = 'on') 
 # Next line is Ultimate feature only
-#shapes = psycochart.create_shape_on_image(figure,axes,col='green')
+shapes = psycochart.create_shape_on_image(figure,axes,col='green')
 axes.plot()
 print('working')
